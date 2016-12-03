612ad9ca3f4b4c616e10d5fcfc1a76ece6ab71b8
mjkim9334 <mjkim9334@naver.com>
2016-12-03 23:27:05 +0900

    Add files via upload

---
+++ Emphasis.java
@@ -0,0 +1,9 @@
+
+public class Emphasis extends Token{
+
+	String text=null;
+	public Emphasis(String line)
+	{
+		text=line;
+	}
+}
---
+++ Token.java
@@ -0,0 +1,11 @@
+
+
+public class Token {
+
+	String text=null;
+	
+	public Token(String text)
+	{
+		this.text=text;
+	}
+}
